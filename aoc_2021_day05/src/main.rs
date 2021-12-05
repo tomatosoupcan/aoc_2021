4832fd1fcb15504b36215a8d6c4de4c9970ce0a0
tSoup <benjamin.chuba@gmail.com>
2021-12-05 00:27:00 -0700

    Update day 5
    Add a modifier to star_1 to solve for star_2 instead of copy pasting

--- aoc_2021_day05/src/main.rs
+++ aoc_2021_day05/src/main.rs
@@ -61,8 +61,8 @@ fn read_a_file(filename: &str) -> std::io::Result<Vec<String>> {
     return Ok(vec)
 }
 
-fn star_1() {
-    let input = read_a_file("C:/input/input.txt").unwrap();
+fn star_1(straight: bool) {
+    let input = read_a_file("C:/input/inputtest.txt").unwrap();
     let mut lines: Vec<Line> = Vec::new();
     for line in input {
         let point1g = line.split(" -> ").collect::<Vec<&str>>()[0];
@@ -107,7 +107,7 @@ fn star_1() {
         for row in 0..max_y.round() as i32 + 1 {
             let mut result = 0;
             for line in &lines {
-                if line.is_straight()
+                if (line.is_straight() || straight)
                     && line.on_line(Point {x: row as f64, y: column as f64}) {
                     result += 1;
                 }
@@ -127,71 +127,11 @@ fn star_1() {
 }
 
 fn star_2() {
-    let input = read_a_file("C:/input/input.txt").unwrap();
-    let mut lines: Vec<Line> = Vec::new();
-    for line in input {
-        let point1g = line.split(" -> ").collect::<Vec<&str>>()[0];
-        let point2g = line.split(" -> ").collect::<Vec<&str>>()[1];
-        let point1x =
-            point1g
-                .split(",")
-                .collect::<Vec<&str>>()[0]
-                .parse::<f64>().unwrap();
-        let point1y =
-            point1g
-                .split(",")
-                .collect::<Vec<&str>>()[1]
-                .parse::<f64>().unwrap();
-        let point2x =
-            point2g
-                .split(",")
-                .collect::<Vec<&str>>()[0]
-                .parse::<f64>().unwrap();
-        let point2y =
-            point2g
-                .split(",")
-                .collect::<Vec<&str>>()[1]
-                .parse::<f64>().unwrap();
-        let point1 = {Point {x: point1x, y:point1y}};
-        let point2 = {Point {x: point2x, y:point2y}};
-        let line_made = {Line {start: point1, end: point2}};
-        lines.push(line_made);
-    }
-    let mut max_x = -f64::INFINITY;
-    let mut max_y = -f64::INFINITY;
-    for line in &lines {
-        if line.start.x > max_x {max_x = line.start.x}
-        if line.start.y > max_y {max_y = line.start.y}
-        if line.end.x > max_x {max_x = line.end.x}
-        if line.end.y > max_y {max_y = line.end.y}
-    }
-
-    let mut result_vector = Vec::new();
-    for column in 0..max_x.round() as i32 + 1 {
-        let mut result_line = Vec::new();
-        for row in 0..max_y.round() as i32 + 1 {
-            let mut result = 0;
-            for line in &lines {
-                if line.on_line(Point {x: row as f64, y: column as f64}) {
-                    result += 1;
-                }
-            }
-            result_line.push(result);
-        }
-        result_vector.push(result_line)
-    }
-    let mut big_intersect = 0;
-    for line in &result_vector {
-        //println!("{:?}", line);
-        big_intersect += line.iter().filter(|x| x > &&1).count();
-    }
-    println!("Number of >2 intersections is {}", big_intersect)
-
-
+    star_1(true)
 }
 
 fn main() {
-    star_1();
+    star_1(false);
     star_2();
     /*let test_line = {Line {start: {Point {x: 7.0, y: 0.0}}, end: {Point {x: 7.0, y: 4.0}}}};
     let test_point = {Point {x: 7.0, y: 3.0}};
