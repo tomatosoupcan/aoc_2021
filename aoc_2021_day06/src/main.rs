2fea1b3077a90c3aff9bb1758a440a9a413f394d
tSoup <benjamin.chuba@gmail.com>
2021-12-06 09:57:45 -0700

    Make it smaller and less readable haha

--- aoc_2021_day06/src/main.rs
+++ aoc_2021_day06/src/main.rs
@@ -18,19 +18,13 @@ fn star_1(days: i64) {
 }
 
 fn star_2(days: i64) {
-    let input = read_a_file("C:/input/input.txt").unwrap();
-    let school: Vec<usize> = input[0].split(",").map(|x| x.parse::<usize>().unwrap()).collect();
-    let mut fish_days: Vec<u64> = vec![0,0,0,0,0,0,0,0,0];
-    for fish in school {
-        fish_days[fish] += 1;
-    }
-    for _day in 1..days+1 {
-        let push_it = fish_days.remove(0);
-        fish_days.push(push_it);
-        fish_days[6] += push_it;
-    }
+    let mut fish_days: Vec<u64> = vec![0;9];
+    read_a_file("C:/input/input.txt").unwrap()[0]
+        .split(",")
+        .map(|x| x.parse::<usize>().unwrap())
+        .for_each(|fish| fish_days[fish] +=1);
+    (0..days).for_each(|_days| { fish_days.rotate_left(1); fish_days[6] += fish_days[8]});
     println!("Fish Count {}", fish_days.iter().sum::<u64>());
-
 }
 
 fn main() {
