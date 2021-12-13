a95ff30becd85ffb602a36bced431951803bc45e
tSoup <benjamin.chuba@gmail.com>
2021-12-13 12:56:31 -0500

    Move the bounds iterator inside of the other for loop to save on time

--- aoc_2021_day13/src/main.rs
+++ aoc_2021_day13/src/main.rs
@@ -15,6 +15,7 @@ fn read_a_file(filename: &str) -> std::io::Result<Vec<String>> {
 }
 
 fn star_1_2() {
+    //file reading
     let input = read_a_file("C:/input/input.txt").unwrap();
     let mut coordinates = HashSet::new();
     let mut folds = Vec::new();
@@ -39,16 +40,28 @@ fn star_1_2() {
             folds.push((x,y));
         }
     }
+    //process data
     let mut first_fold = true;
+    let mut max_x = i32::MIN;
+    let mut max_y = i32::MIN;
+    let final_fold = folds[folds.len()-1];
     for fold in folds {
         for coord in coordinates.clone() {
-            if fold.0 == 'y' && coord.1 > fold.1 {
+            let mut temp_x = coord.0;
+            let mut temp_y = coord.1;
+            if fold.0 == 'y' && coord.1 > fold.1  {
+                temp_y = fold.1-((coord.1-fold.1)%(fold.1+1));
                 coordinates.remove(&coord);
-                coordinates.insert((coord.0, fold.1-((coord.1-fold.1)%(fold.1+1))));
+                coordinates.insert((coord.0, temp_y));
             }
             if fold.0 == 'x' && coord.0 > fold.1 {
+                temp_x = fold.1-((coord.0-fold.1)%(fold.1+1));
                 coordinates.remove(&coord);
-                coordinates.insert((fold.1-((coord.0-fold.1)%(fold.1+1)), coord.1));
+                coordinates.insert((temp_x, coord.1));
+            }
+            if fold == final_fold {
+                if temp_x > max_x { max_x = temp_x }
+                if temp_y > max_y { max_y = temp_y }
             }
         }
         if first_fold {
@@ -56,18 +69,12 @@ fn star_1_2() {
             println!("First Fold Count: {}", coordinates.len());
         }
     }
-    let mut max_x = i32::MIN;
-    let mut max_y = i32::MIN;
-    for coord in &coordinates {
-        if coord.0 > max_x {max_x = coord.0}
-        if coord.1 > max_y {max_y = coord.1}
-    }
     for y in 0..(max_y+1) as usize {
         for x in 0..(max_x+1) as usize {
             let mut print = ' ';
             for coord in &coordinates {
                 if coord == &(x as i32, y as i32) {
-                    print = '#'
+                    print = '▓'
                 }
             }
             print!("{}", print);
