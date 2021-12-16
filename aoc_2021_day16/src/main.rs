49409105404b65f1d7300309dff5c497f73ce06b
tSoup <benjamin.chuba@gmail.com>
2021-12-16 15:23:39 -0500

    Adding comments for when I inevitably need this later

--- aoc_2021_day16/src/main.rs
+++ aoc_2021_day16/src/main.rs
@@ -14,17 +14,17 @@ fn read_a_file(filename: &str) -> std::io::Result<Vec<String>> {
 }
 #[derive(Debug)]
 struct TrData {
-    ver: u8,
-    id: u8,
-    subs: Vec<TrData>,
-    lt_id: u8,
-    literal: i64,
+    ver: u8,           //the version id
+    id: u8,            //the type id
+    subs: Vec<TrData>, //a vector of trs contained by this tr
+    lt_id: u8,         //the lt id, where applicable
+    literal: i64,      //the literal value, where applicable
 }
 
 fn hex_2_binary(hex: &Vec<char>) -> Vec<u8> {
     let mut output = Vec::new();
-    let i = 1 as u8;
-    let o = 0 as u8;
+    let i = 1 as u8; //this saves time
+    let o = 0 as u8; //having to type u8 over and over
     for chr in hex {
         output.extend(match chr {
             '0' => vec![o, o, o, o],
@@ -52,17 +52,20 @@ fn hex_2_binary(hex: &Vec<char>) -> Vec<u8> {
 fn binary_2_struct(bin: &mut Vec<u8>, total: &mut i32) -> TrData {
     let mut subs = Vec::new();
     let mut ver = 0;
+    //separate off the version number
     for _ in 0..3 {
         ver <<= 1;
         ver ^= bin.remove(0);
     }
     *total += ver as i32;
     let mut id = 0;
+    //separate off the id
     for _ in 0..3 {
         id <<= 1;
         id ^= bin.remove(0);
     }
     let mut lt_id = 0;
+    //if not id 4, we need to get the lt_id, if 4 store a placeholder for lt_id
     if id != 4 {
         lt_id <<= 1;
         lt_id ^= bin.remove(0);
@@ -70,6 +73,9 @@ fn binary_2_struct(bin: &mut Vec<u8>, total: &mut i32) -> TrData {
         lt_id = u8::MAX;
     }
     let mut literal: i64 = 0;
+    //if lt_id is the max u8 value, that means it is not present for this operation
+    //all we need to do is find where the transmission ends, strip the identifiers
+    //and concatenate by pushing bits onto the value, then store this as the literal
     if lt_id == u8::MAX {
         let mut finding = true;
         let mut depth = 0;
@@ -89,7 +95,11 @@ fn binary_2_struct(bin: &mut Vec<u8>, total: &mut i32) -> TrData {
                 literal ^= bin.remove(0) as i64;
             };
         }
-    } else if lt_id == 0 {
+    }
+    //if lt_id is 0, we need to parse a specific range as transmissions
+    //this is easy enough to do simply by popping off the range and running it
+    //through this function recursively
+    else if lt_id == 0 {
         let bit_count = 15;
         let mut sp_len = 0;
         for _ in 0..bit_count {
@@ -103,7 +113,12 @@ fn binary_2_struct(bin: &mut Vec<u8>, total: &mut i32) -> TrData {
         while new_vec.iter().map(|x| *x as i32).sum::<i32>() != 0 {
             subs.push(binary_2_struct(&mut new_vec, total));
         }
-    } else if lt_id == 1 {
+    }
+    //if lt_id is 1, we need to split it into the proper number of transmissions
+    //we do this by simply passing the binary vector a number of times equal to
+    //the number of transmissions identified in the 11 bit identifier
+    //since they will already terminate themselves accordingly each time
+    else if lt_id == 1 {
         let bit_count = 11;
         let mut sp_count = 0;
         for _ in 0..bit_count {
@@ -127,14 +142,14 @@ fn pro(structure: &TrData) -> i64 {
     let subs_s = &structure.subs;
     let subs = subs_s.iter().map(|x| pro(x));
     return match &structure.id {
-        0 => subs.fold(0, |acc, x| x + acc),
-        1 => subs.fold(1, |acc, x| x * acc),
-        2 => subs.min().unwrap(),
-        3 => subs.max().unwrap(),
-        5 => (pro(&subs_s[0]) > pro(&subs_s[1])) as i64,
-        6 => (pro(&subs_s[0]) < pro(&subs_s[1])) as i64,
-        7 => (pro(&subs_s[0]) == pro(&subs_s[1])) as i64,
-        _ => structure.literal,
+        0 => subs.fold(0, |acc, x| x + acc),              //0 is addition
+        1 => subs.fold(1, |acc, x| x * acc),              //1 is multiplication
+        2 => subs.min().unwrap(),                         //2 is minimum value
+        3 => subs.max().unwrap(),                         //3 is max value
+        5 => (pro(&subs_s[0]) > pro(&subs_s[1])) as i64,  //5 is greater than
+        6 => (pro(&subs_s[0]) < pro(&subs_s[1])) as i64,  //6 is less than
+        7 => (pro(&subs_s[0]) == pro(&subs_s[1])) as i64, //7 is equal to
+        _ => structure.literal,                           //all else return the value
     };
 }
 
